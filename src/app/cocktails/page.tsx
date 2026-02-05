d9cdd5985042e1268fb8a6655a9f6ef428a4b309
Jinjas <rvcnovo@gmail.com>
2026-02-05 17:53:09 +0000

    cocktail list page

--- src/app/cocktails/page.tsx
+++ src/app/cocktails/page.tsx
@@ -1,61 +1,72 @@
 "use client";
 
 import Sidebar from "../components/Sidebar";
-
 import { useState } from "react";
 import { useAppContext } from "../context/AppContext";
-import { Ingredient } from "../types";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+
 export default function CocktailsPage() {
-  const { cocktails, addCocktail, ingredients, addIngredientToCocktail } =
-    useAppContext();
+  const { cocktails, addCocktail } = useAppContext();
 
   const [cocktailName, setCocktailName] = useState("");
-  const [selectedIngredientId, setSelectedIngredientId] = useState("");
-
-  function getIngredient(): Ingredient | undefined {
-    return ingredients.find((i) => i.id === selectedIngredientId);
-  }
 
   return (
     <div className="flex h-screen w-full">
       <Sidebar />
 
       <main className="flex flex-1">
-        <section className="flex-1 p-10 bg-gray-100 text-black flex flex-col justify-center">
+        <section className="flex-1 p-9 bg-gray-100 text-black flex flex-col w-full h-screen">
           <h1 className="text-2xl font-bold">Cocktails</h1>
-          <div className="flex gap-2 mt-4">
+
+          <div className="flex gap-2 pt-4 w-full pr-2">
             <input
               value={cocktailName}
               onChange={(e) => setCocktailName(e.target.value)}
               placeholder="Cocktail name..."
-              className="border p-2 rounded"
+              className="border p-2 rounded flex-1"
             />
 
             <button
               onClick={() => {
-                addCocktail(cocktailName);
-                setCocktailName("");
+                if (cocktailName.trim() !== "") {
+                  const id = addCocktail(cocktailName);
+                  setCocktailName("");
+                  redirect(`/cocktails/${id}`);
+                }
               }}
-              className="bg-black text-white px-4 rounded"
+              className="block bg-gray-700 hover:bg-gray-800 text-white px-4 rounded"
             >
               Add Cocktail
             </button>
           </div>
 
-          <div className="mt-6">
-            <select
-              value={selectedIngredientId}
-              onChange={(e) => setSelectedIngredientId(e.target.value)}
-              className="border p-2 rounded"
-            >
-              <option value="">Select ingredient...</option>
-              {ingredients.map((ing) => (
-                <option key={ing.id} value={ing.id}>
-                  {ing.name}
-                </option>
-              ))}
-            </select>
-          </div>
+          <ul className="pt-4 overflow-y-auto">
+            {cocktails.map((i) => (
+              <li key={i.id}>
+                <div
+                  className="
+                  grid
+                  grid-cols-[2fr_80px]
+                  items-center
+                  gap-2
+                  py-1
+                  px-2
+                  border-b
+                "
+                >
+                  <h3 className="font-semibold">{i.name}</h3>
+                  {/* <h3 className="text-md">{LanguagesList <=3 elems  }...</h3> */}
+                  <Link
+                    href={`/cocktails/${i.id}`}
+                    className=" bg-gray-700 hover:bg-gray-800 text-white px-2 py-1 rounded border text-center"
+                  >
+                    Edit
+                  </Link>
+                </div>
+              </li>
+            ))}
+          </ul>
         </section>
 
         <section className="flex-1 p-10 bg-white text-black border-l border-gray-300 flex flex-col justify-center">
