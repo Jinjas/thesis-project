3703c7b493c50be8bf86f78fdf93c3beaadb64d8
Jinjas <rvcnovo@gmail.com>
2026-03-16 17:03:03 +0000

    bugfix(table Content): now the content is not duplicated

--- src/app/components/areas/TableData.tsx
+++ src/app/components/areas/TableData.tsx
@@ -9,7 +9,7 @@ usage:
 import { useAppContext } from "../../context/AppContext";
 import { useParams } from "next/navigation";
 import SectionTable from "./TableArea";
-import { TableDict, Ingredient } from "../../types";
+import { TableDict, Ingredient, Cocktail } from "../../types";
 
 type Props = {
   type: number;
@@ -30,6 +30,50 @@ const tableDict: TableDict[] = [
   },
 ];
 
+function groupTableOfCocktail(cocktail: Cocktail, ingredients: Ingredient[]) {
+  let counter = 1;
+  const cockIngredients: Ingredient[] = Object.entries(cocktail.ingredients)
+    .filter(([_, v]) => v)
+    .map(([ingId]) => ingredients.find((i) => i.id === ingId))
+    .filter((i): i is (typeof ingredients)[number] => !!i);
+
+  const sectionMap: Record<string, TableDict> = {};
+  for (const ingredient of cockIngredients) {
+    for (const table of ingredient.table) {
+      if (!sectionMap[table.section]) {
+        sectionMap[table.section] = {
+          section: table.section,
+          rows: table.rows.map((row) => {
+            const newRow = [...row];
+            newRow[0] = "0";
+            newRow[newRow.length - 1] = "--";
+            return newRow;
+          }),
+        };
+      } else {
+        const existingRows = sectionMap[table.section].rows;
+        for (const row of table.rows) {
+          const newRow = [...row];
+          newRow[0] = "0";
+          newRow[newRow.length - 1] = "--";
+          if (!existingRows.some((r) => r.join() === newRow.join())) {
+            existingRows.push(newRow);
+          }
+        }
+      }
+    }
+  }
+  const data: TableDict[] = Object.entries(sectionMap).map(([s, r]) => ({
+    section: s,
+    rows: r.rows.map((row) => {
+      row[0] = String(counter++);
+      return row;
+    }),
+  }));
+
+  return data;
+}
+
 export default function TableData({ type, selectedId }: Props) {
   const { ingredients, cocktails } = useAppContext();
   const { id } = useParams();
@@ -37,6 +81,7 @@ export default function TableData({ type, selectedId }: Props) {
   let data: TableDict[] | undefined = [];
 
   switch (type) {
+    //individual detail pages
     case 0: {
       const ingredient = ingredients.find((c) => c.id === id);
       data = ingredient?.table;
@@ -48,28 +93,13 @@ export default function TableData({ type, selectedId }: Props) {
     case 1: {
       const cocktail = cocktails.find((c) => c.id === id);
       if (!cocktail) break;
-
-      let counter = 1;
-
-      const data: TableDict[] = Object.entries(cocktail.ingredients)
-        .filter(([_, v]) => v)
-        .map(([ingId]) => ingredients.find((i) => i.id === ingId))
-        .filter((i): i is (typeof ingredients)[number] => !!i)
-        .flatMap((ingredient) =>
-          ingredient.table.map((section) => ({
-            section: section.section,
-            rows: section.rows.map((row) => {
-              const newRow = [...row];
-              newRow[0] = String(counter++);
-              return newRow;
-            }),
-          })),
-        );
+      data = groupTableOfCocktail(cocktail, ingredients);
 
       if (data) return <SectionTable data={data} />;
       break;
     }
 
+    //pages with lists
     case 2: {
       const ingredient = ingredients.find((c) => c.id === selectedId);
       data = ingredient?.table;
@@ -81,33 +111,16 @@ export default function TableData({ type, selectedId }: Props) {
     case 3: {
       const cocktail = cocktails.find((c) => c.id === selectedId);
       if (!cocktail) break;
-
-      let counter = 1;
-
-      const data: TableDict[] = Object.entries(cocktail.ingredients)
-        .filter(([_, v]) => v)
-        .map(([ingId]) => ingredients.find((i) => i.id === ingId))
-        .filter((i): i is (typeof ingredients)[number] => !!i)
-        .flatMap((ingredient) =>
-          ingredient.table.map((section) => ({
-            section: section.section,
-            rows: section.rows.map((row) => {
-              const newRow = [...row];
-              newRow[0] = String(counter++);
-              return newRow;
-            }),
-          })),
-        );
+      data = groupTableOfCocktail(cocktail, ingredients);
 
       if (data) return <SectionTable data={data} />;
       break;
     }
 
+    //individual ingredients of cocktail
     case 4: {
       const cocktail = cocktails.find((c) => c.id === id);
       if (!cocktail) break;
-
-      let counter = 1;
 
       const activeIngredients: Ingredient[] = Object.entries(
         cocktail.ingredients,
@@ -129,11 +142,10 @@ export default function TableData({ type, selectedId }: Props) {
         );
       break;
     }
+
     case 5: {
       const cocktail = cocktails.find((c) => c.id === selectedId);
       if (!cocktail) break;
-
-      let counter = 1;
 
       const activeIngredients: Ingredient[] = Object.entries(
         cocktail.ingredients,
