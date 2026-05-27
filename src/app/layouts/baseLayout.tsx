ff794a9f8965bde2fb91491675f902b4853c7ca7
Jinjas <rvcnovo@gmail.com>
2026-05-27 01:06:50 +0100

    bugfix(cocktailVisualisation on cocktail list): there were two issues, first the visualizations was not called and the cocktailId was not known on that page level so it was needed to be sent to the base layout

--- src/app/layouts/baseLayout.tsx
+++ src/app/layouts/baseLayout.tsx
@@ -4,16 +4,17 @@ import { Sidebar, VizBar } from "../components";
 type Props = {
   children: React.ReactNode;
   withViz?: boolean;
+  selectedId?: string;
 };
 
-export default function BaseLayout({ children, withViz }: Props) {
+export default function BaseLayout({ children, withViz, selectedId }: Props) {
   return (
     <div className="flex h-screen w-full cursor-default">
       <Sidebar />
 
       <main className="flex flex-1">{children}</main>
 
-      {withViz && <VizBar />}
+      {withViz && <VizBar selectedId={selectedId || ""} />}
     </div>
   );
 }
